b2af35ce8d4500f6696c8b24aede760bed9aac5b
Valentin Lageard <valentin.lageard.pro@gmail.com>
2021-09-10 18:42:23 +0200

    ex00 done, maybe needs to add some tests ?

---
+++ ex00/whatever.hpp
@@ -0,0 +1,23 @@
+#ifndef whatever_HPP
+# define whatever_HPP
+
+
+template <typename T>
+void swap(T & x, T & y) {
+	T tmp(x);
+
+	x = y;
+	y = tmp;
+}
+
+template <typename T>
+T & max(T & x, T & y) {
+	return (x > y) ? x : y;
+}
+
+template <typename T>
+T & min(T & x, T & y) {
+	return (x < y) ? x : y;
+}
+
+#endif
